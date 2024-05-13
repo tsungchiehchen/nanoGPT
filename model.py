71c4c55f5928947074d1c06689f16d9ccff80c1c
Tsung-Chieh Chen <d12300d@gmail.com>
2024-05-13 11:30:55 -0700

    Q3 Attempt 4 - fix wind pass in

--- model.py
+++ model.py
@@ -28,10 +28,10 @@ class LayerNorm(nn.Module):
 
 class CausalSelfAttention(nn.Module):
 
-    def __init__(self, config, wind):
+    def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
-        self.wind = wind
+        self.wind = config.wind
         # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
         # output projection
