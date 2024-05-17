071696c4fe16408268f505b865bf86f908d95847
Tsung-Chieh Chen <d12300d@gmail.com>
2024-05-17 14:16:44 -0700

    Q2 Attempt 2

--- model.py
+++ model.py
@@ -39,18 +39,26 @@ class CausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
-        # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
-        # output projection
-        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        self.key_dim = config.key_dim if config.key_dim != 64 else config.n_embd // config.n_head # Question 2
+        self.query_dim = config.query_dim if config.query_dim != 64 else config.n_embd // config.n_head # Question 2
+        # self.value_dim = config.n_embd
+
+        if self.key_dim == 64 and self.query_dim == 64:
+            # key, query, value projections for all heads, but in a batch
+            self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+            # output projection
+            self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        else:
+            # key, query, value projections for all heads, but in a batch
+            self.c_attn = nn.Linear(config.n_embd, self.query_dim * config.n_head + self.key_dim * config.n_head + config.n_embd, bias=config.bias)
+            # output projection
+            self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
         # regularization
         self.attn_dropout = nn.Dropout(config.dropout)
         self.resid_dropout = nn.Dropout(config.dropout)
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.dropout = config.dropout
-        self.key_dim = config.key_dim if config.key_dim != 64 else config.n_embd // config.n_head # Question 2
-        self.query_dim = config.query_dim if config.query_dim != 64 else config.n_embd // config.n_head # Question 2
         self.wind = config.wind # Question 3
         self.abs = config.abs # Question 7
         # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
@@ -72,9 +80,9 @@ class CausalSelfAttention(nn.Module):
             v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
         else:
             # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-            q, k, v  = self.c_attn(x).split([self.query_dim, self.key_dim, self.n_embd], dim=2)
-            k = k.view(B, T, self.n_head, self.key_dim // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-            q = q.view(B, T, self.n_head, self.query_dim // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+            q, k, v  = self.c_attn(x).split([self.query_dim * self.n_head, self.key_dim * self.n_head, self.n_embd], dim=2)
+            k = k.view(B, T, self.n_head, self.key_dim).transpose(1, 2) # (B, nh, T, hs)
+            q = q.view(B, T, self.n_head, self.query_dim).transpose(1, 2) # (B, nh, T, hs)
             v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
         if self.wind != 256:
@@ -112,7 +120,7 @@ class CausalSelfAttention(nn.Module):
             if self.key_dim == 64 and self.query_dim == 64:
                 y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
             else:
-                y = y.transpose(1, 2).contiguous().view(B, T, self.key_dim * self.n_head) # re-assemble all head outputs side by side
+                y = y.transpose(1, 2).contiguous().view(B, T, self.n_embd) # re-assemble all head outputs side by side
         else:
             # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
             if self.flash and not self.abs:
@@ -129,7 +137,7 @@ class CausalSelfAttention(nn.Module):
             if self.key_dim == 64 and self.query_dim == 64:
                 y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
             else:
-                y = y.transpose(1, 2).contiguous().view(B, T, self.key_dim * self.n_head) # re-assemble all head outputs side by side
+                y = y.transpose(1, 2).contiguous().view(B, T, self.n_embd) # re-assemble all head outputs side by side
 
         # output projection
         y = self.resid_dropout(self.c_proj(y))
